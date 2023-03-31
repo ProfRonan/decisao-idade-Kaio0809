08bcb183a73ca20fa6463e4384f4596953f4bc94
Kaio <128155244+Kaio0809@users.noreply.github.com>
2023-03-31 14:40:14 +0000

    correção

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-impossivel=False
-menor=False
-maior=False
-velho=False
 
 
 idade = int(input())
 if idade < 0:
-    print('Impossível!')
-    impossivel = True
+    print('impossível!')
+    
 if idade < 18:
     print('não precisa se alistar.')
-    menor = True
-if 18 < idade > 65:
-    print('não esqueça de votar na proxima eleição.')
-    maior = True
-if idade > 65:
-    print('vá descansar.')
-    velho = True
-if not(impossivel or menor or maior or velho):
+
+elif 18 < idade < 65:
+    print('Não esqueça de votar na proxima eleição.')
+
+elif idade > 65:
+    print('Vá descansar.')
+
+else:
     print('eita!')
