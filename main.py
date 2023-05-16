dbe6b94b6b257007d8462b650b24708310d9039b
Kaio <128155244+Kaio0809@users.noreply.github.com>
2023-05-16 11:42:57 +0000

    exercicio concluído

--- main.py
+++ main.py
@@ -3,15 +3,15 @@
 idade = int(input())
 if idade < 0:
     print('Impossível!')
-    
-if idade < 18:
-    print('Não precisa se alistar.')
-
-elif 18 < idade < 65:
-    print('Não esqueça de votar na proxima eleição.')
-
-elif idade > 65:
-    print('Vá descansar.')
-
 else:
-    print('Eita!')
+    if idade >= 0 and idade < 18:
+        print('Não precisa se alistar.')
+
+    elif 18 < idade < 65:
+        print('Não esqueça de votar na próxima eleição.')
+
+    elif idade > 65:
+        print('Vá descansar.')
+
+    else:
+        print('Eita!')
